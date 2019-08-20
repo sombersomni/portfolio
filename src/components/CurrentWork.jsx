5336afa62a0d2689bd70cbe65c4c7c3a407ff344
somberSomni <kartunehustla@gmail.com>
2019-08-19 22:49:40 -0500

    Working on coding projects page. Styling the featured work section

--- src/components/CurrentWork.jsx
+++ src/components/CurrentWork.jsx
@@ -1,38 +1,54 @@
 import React from 'react';
 import ArrowButton from './ArrowButton.jsx';
 import styled from 'styled-components';
+import Chip from '@material-ui/core/Chip';
 
 const WorkContainer = styled.div`
     width: 100%;
     height: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: flex-start;
     align-items: center;
     background: yellow;
+    padding-top: 80px;
 `;
 
 const WorkTitle = styled.div`
-    width: 400px;
-    height: 400px;
-    background: red;
+    width: 25%;
+    min-width: 300px;
+    min-height: 75vh;
+    background: white;
+`;
 
+const WorkHeader = styled.div`
+    width: 70%;
+    background: url(${props => props.bg});
 `;
 
 const Work = styled.div`
-    width: 80vw;
+    width: 90vw;
     display: flex;
     flex-direction: row;
 `;
-export default function CurrentWork() {
+export default function CurrentWork({desc, title, headerImg, tagline}) {
     return (
         <WorkContainer>
             <Work>
                 <WorkTitle>
-                    <h1>Encage</h1>
-                    <h2>Protect Your Instances</h2>
+                    <h1>{title}</h1>
+                    <p style={{ fontWeight: 'bold', padding: 25 }}>{tagline}</p>
+                    <p style={{ padding: 25 } }>{desc}</p>
+                    <Chip
+                        label="Visit Website"
+                        component="a"
+                        href="https://pick-flicks-app.herokuapp.com"
+                        target="_blank"
+                        style={{ marginBottom: 20 }}
+                        clickable
+                    />
                 </WorkTitle>
-
+                <WorkHeader bg={headerImg} />
             </Work>
             <ArrowButton label='See More' />
         </WorkContainer> 
--- src/pages/Code.jsx
+++ src/pages/Code.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
-
+import pick_flicks_header from '../imgs/projects/pick_flicks_header.png';
 //components
 import { Container, FeatureContainer} from '../components/Containers.jsx';
 import CurrentWork from '../components/CurrentWork.jsx';
 import Feature from '../components/Feature.jsx';
 
+const currentWork = {
+    headerImg: pick_flicks_header,
+    desc: "Duis eu convallis leo. Etiam varius vehicula augue ut viverra. Ut placerat orci vel commodo pellentesque. Pellentesque maximus malesuada lacus non venenatis. Vivamus eleifend cursus dui, eu rutrum nibh tempus non. Quisque at nisi rhoncus, pellentesque lacus vel, pretium turpis. Nunc vitae placerat leo.",
+    tagline: "A movie website built using MovieDB API",
+    title: "Pick Flicks"
+}
 function Code() {
     return (
         <Container>
             <FeatureContainer bgcolor={'#CCC'}>
-                <CurrentWork />
+                <CurrentWork {...currentWork} />
             </FeatureContainer>
         </Container>
     );
